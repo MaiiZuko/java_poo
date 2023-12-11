a5fabffaf9eb87c7c3a4b9ae4c65bf0cd280e799
MaiiZuko <maiaramzucco@gmail.com>
2023-12-11 20:43:33 -0300

    media

---
+++ media.java
@@ -0,0 +1,18 @@
+package media_artimetica;
+
+import java.util.Scanner; //Usada para ler entrada do teclado
+
+public class media {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //Criando o objeto scanner para ler entrada do teclado
+        double x, y, media;
+
+        System.out.print("Digite o primeiro número: ");
+        x = sc.nextDouble(); //Le a próxima entrada do teclado e a converte em um número do tipo double
+        System.out.print("Digite o segundo número: ");
+        y = sc.nextDouble();
+        media = (x+y)/ 2.0;
+        System.out.print("Media = " + media);
+        sc.close(); //Scanner é fechado após o número ter sido lido no teclado
+    }
+}
