67619935b6f110f2a4b859dbbf52e29730e52624
MaiiZuko <maiaramzucco@gmail.com>
2024-02-09 10:26:51 -0300

    Escopo

---
+++ java_poo/Escopo.java
@@ -0,0 +1,17 @@
+package java_poo;
+
+public class Escopo {
+    
+    public static void main(String[] args) {
+        
+        double price = 400;
+        double discount;
+
+        if (price < 200.00) {
+            discount = price * 0.1;
+        } else {
+            discount = 0;
+        }
+        System.out.println(discount);
+    }
+}
