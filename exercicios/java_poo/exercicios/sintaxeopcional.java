b3ac2ef619dbfac8a150cf1c5397c11c786844da
MaiiZuko <maiaramzucco@gmail.com>
2024-02-09 09:43:58 -0300

    Sintaxe

---
+++ exercicios/java_poo/exercicios/sintaxeopcional.java
@@ -0,0 +1,23 @@
+package java_poo.exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class sintaxeopcional {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US); //convenções dos Estados Unidos
+        Scanner sc = new Scanner(System.in);
+
+        int minutos = sc.nextInt();
+
+        double conta = 50.0;
+
+        if (minutos > 100) {
+            conta = conta + (minutos - 100) *2;
+        }
+
+        System.out.println(conta);
+        sc.close();
+    }
+    
+}
